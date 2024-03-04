0dc88ef1371cb556011d5d7035744202031d1364
heroadn <benbenjamin554@gmail.com>
2024-03-04 19:25:57 -0300

    migrated to vpkg, updated the code, and added keys at the start of the game

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Was done with the tutorias of https://www.gamefromscratch.com/page/SFML-CPP-Tuto
 
 # Images
 The image of the bat was from https://opengameart.org/content/castle-platformer the author Jetrel.
+Keys by https://gerald-burke.itch.io/geralds-keys the author Gerald
 
 # Building
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,37 +3,99 @@
 #include <random>
 #include <unordered_map>
 
+const int WIDTH  = 640;
+const int HEIGHT = 480;
+
+struct Entity
+{
+    sf::Sprite sprite;
+    sf::Image image;
+    sf::Texture texture;
+    sf::IntRect frame;
+
+    float x = 0.0f;
+    float y = 0.0f;
+    float speed = 0.0f;
+};
+
 int getRow(int numberOfRow);
 sf::Image removeColor(sf::Image image, int width, int height, int r, int g, int b);
 
 int main(int argc, char** argv) {
-    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Demo");
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "SFML Demo");
     sf::Event event;
     sf::Clock clock;
-    sf::Texture texture;
-    sf::Sprite sprite;
-    sf::Image image;
-    float x = 0.0f;
-    float y = 0.0f;
 
-    /*SPRITESHEET*/
-    int row = getRow(0);
-    sf::IntRect rectSourceSprite(row, 3, 30, 30);
+    //
+    Entity bat;
+    bat.speed = 300.0f;
+    
+    //
+    Entity buttons[4];//w - s - d - a
 
     /*The author of the image(" bat.png ")    is "Jetrel"    taken from URL(" https://opengameart.org/content/castle-platformer ") */
-    /*The author of the image(" arcade.png ") is "OptimusDu" taken FROM URL(" https://opengameart.org/content/pixel-arcade-buttons ") */
-    if (image.loadFromFile("assets/bat.png"))
-    {
-        texture.loadFromImage(removeColor(image, 171, 103, 111, 109, 81));
-        
-
-        sprite.setTexture(texture);
-        sprite.setTextureRect(rectSourceSprite);
-        sprite.setScale(2, 2);
-    }
-    else {
+    /*The author of the image keys            is "Gerald"    taken from URL(" https://gerald-burke.itch.io/geralds-keys ") */
+    if (!bat.image.loadFromFile(ASSETS_PATH"bat.png"))
         std::cout << "Could not load image" << std::endl;
-    }
+
+    if (!buttons[0].image.loadFromFile(ASSETS_PATH"Gerald_Keys/Keys/W-Key.png"))
+        std::cout << "Could not load image" << std::endl;
+
+    if (!buttons[1].image.loadFromFile(ASSETS_PATH"Gerald_Keys/Keys/S-Key.png"))
+        std::cout << "Could not load image" << std::endl;
+
+    if (!buttons[2].image.loadFromFile(ASSETS_PATH"Gerald_Keys/Keys/D-Key.png"))
+        std::cout << "Could not load image" << std::endl;
+
+    if (!buttons[3].image.loadFromFile(ASSETS_PATH"Gerald_Keys/Keys/A-Key.png"))
+        std::cout << "Could not load image" << std::endl;
+
+    //bat
+    bat.frame = sf::IntRect(getRow(0), 3, 30, 30);
+    bat.texture.loadFromImage(removeColor(bat.image, 171, 103, 111, 109, 81));
+    bat.sprite.setTexture(bat.texture);
+    bat.sprite.setTextureRect(bat.frame);
+    bat.sprite.setScale(2, 2);
+
+    //button_w
+    buttons[0].frame = sf::IntRect(0, 0, 32, 32);
+    buttons[0].texture.loadFromImage(buttons[0].image);
+    buttons[0].sprite.setTexture(buttons[0].texture);
+    buttons[0].sprite.setTextureRect(buttons[0].frame);
+    buttons[0].sprite.setScale(2, 2);
+
+    //button_s
+    buttons[1].frame = sf::IntRect(0, 0, 32, 32);
+    buttons[1].texture.loadFromImage(buttons[1].image);
+    buttons[1].sprite.setTexture(buttons[1].texture);
+    buttons[1].sprite.setTextureRect(buttons[1].frame);
+    buttons[1].sprite.setScale(2, 2);
+
+    //button_d
+    buttons[2].frame = sf::IntRect(0, 0, 32, 32);
+    buttons[2].texture.loadFromImage(buttons[2].image);
+    buttons[2].sprite.setTexture(buttons[2].texture);
+    buttons[2].sprite.setTextureRect(buttons[2].frame);
+    buttons[2].sprite.setScale(2, 2);
+
+    //button_a
+    buttons[3].frame = sf::IntRect(0, 0, 32, 32);
+    buttons[3].texture.loadFromImage(buttons[3].image);
+    buttons[3].sprite.setTexture(buttons[3].texture);
+    buttons[3].sprite.setTextureRect(buttons[3].frame);
+    buttons[3].sprite.setScale(2, 2);
+
+    //setting bat to start at cener of the screen
+    bat.x = WIDTH / 2;
+    bat.y = HEIGHT / 2;
+    buttons[0].sprite.setPosition((WIDTH / 2), (HEIGHT / 2) - 30);
+    buttons[1].sprite.setPosition((WIDTH / 2), (HEIGHT / 2) + 64 - 30);
+    buttons[2].sprite.setPosition((WIDTH / 2) + 64, (HEIGHT / 2));
+    buttons[3].sprite.setPosition((WIDTH / 2) - 64, (HEIGHT / 2));
+
+    //timing
+    sf::Clock delta;
+    float dt = delta.restart().asSeconds();
 
     while (window.isOpen()) {
         while (window.pollEvent(event)) {
@@ -50,27 +112,27 @@ int main(int argc, char** argv) {
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-            x = x - 0.1f;
+            bat.x -= dt * bat.speed;
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-            x = x + 0.1f;
+            bat.x += dt * bat.speed;
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-            y = y - 0.1f;
+            bat.y -= dt * bat.speed;
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-            y = y + 0.1f;
+            bat.y += dt * bat.speed;
         }
 
         if (clock.getElapsedTime().asSeconds()) {
             int limit = getRow(3);
             int index = clock.getElapsedTime().asSeconds();
 
-            rectSourceSprite.left = getRow(index);
-            sprite.setTextureRect(rectSourceSprite);   
+            bat.frame.left = getRow(index);
+            bat.sprite.setTextureRect(bat.frame);
         }
 
         //RESTART CLOCK
@@ -78,10 +140,19 @@ int main(int argc, char** argv) {
             clock.restart();
         }
 
-        sprite.setPosition(x, y);
-        window.clear();
-        window.draw(sprite);
+        //
+        bat.sprite.setPosition(bat.x, bat.y);
+
+        //
+        window.clear(sf::Color::White);
+        window.draw(buttons[0].sprite);
+        window.draw(buttons[1].sprite);
+        window.draw(buttons[2].sprite);
+        window.draw(buttons[3].sprite);
+
+        window.draw(bat.sprite);
         window.display();
+        dt = delta.restart().asSeconds();
     }
 
 }
